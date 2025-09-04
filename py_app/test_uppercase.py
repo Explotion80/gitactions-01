ba0434a59fb436f81f0cea98ca808635b41e99c4
Explotion80 <39049846+Explotion80@users.noreply.github.com>
2025-09-04 11:18:43 +0200

    update test

--- py_app/test_uppercase.py
+++ py_app/test_uppercase.py
@@ -1,13 +1,13 @@
 import pytest
-from uppercase import to_uppercase
+from uppercase import to_upper
 
 def test_to_uppercase():
-    assert to_uppercase("hello") == "HELLO"
-    assert to_uppercase("world") == "WORLD"
-    assert to_uppercase("Python") == "PYTHON"
-    assert to_uppercase("123abc") == "123ABC"
-    assert to_uppercase("!@#") == "!@#"
-    assert to_uppercase("") == ""
+    assert to_upper("hello") == "HELLO"
+    assert to_upper("world") == "WORLD"
+    assert to_upper("Python") == "PYTHON"
+    assert to_upper("123abc") == "123ABC"
+    assert to_upper("!@#") == "!@#"
+    assert to_upper("") == ""
 
 if __name__ == "__main__":
     pytest.main()
